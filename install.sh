aa07c636ff17cfc70a80e2a7bc579c5b97ace62e
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2019-03-13 13:02:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,22 @@ DOTPATH=~/.dotfiles
 
 # git が使えるなら git
 
-if has "git"; then
+if type "git" > /dev/null 2>&1
+then
     git clone --recursive "$GITHUB_URL" "$DOTPATH"
 
 # 使えない場合は curl か wget を使用する
-elif has "curl" || has "wget"; then
+elif type "curl" || type "wget" > /dev/null 2>&1
+then
     tarball="https://github.com/WakuwakuP/dotfiles/archive/master.tar.gz"
 
     # どっちかでダウンロードして，tar に流す
-    if has "curl"; then
+    if type "curl" > /dev/null 2>&1
+    then
         curl -L "$tarball"
 
-    elif has "wget"; then
+    elif type "wget"> /dev/null 2>&1
+    then
         wget -O - "$tarball"
 
     fi | tar zxv
